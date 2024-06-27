794619896a987a6aa09de7858c4bc8c8f4c48e56
12Me21 <12Me21.mc@gmail.com>
2024-06-26 20:28:21 -0400

    pinwhy

--- lib.js
+++ lib.js
@@ -89,7 +89,7 @@ class Component {
 		for (let p of def.pins) {
 			let px = x + p.r.x
 			let py = y + p.r.y
-			s_pins += `<path class=pin d="m${spacexy(px,py)}${p.r.dir}" />`
+			s_pins += `m${spacexy(px,py)}${p.r.dir}`
 			let pname = p.name
 			let ext = ""
 			if (pname[0]=="~") {
